95d8411871957440737589f0b28b3a70f1785d39
Megan Sullivan <megan@gatsbyjs.com>
2021-03-02 11:07:19 -0800

    feat: add /about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>My name is Megan. I like teaching, video games, and dogs!</p>
+    </main>
+  )
+}
+
+export default AboutPage
