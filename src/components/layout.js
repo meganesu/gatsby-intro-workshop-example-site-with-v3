1c13573d40f47c6beee5780eb708733e953b01b1
Megan Sullivan <megan@gatsbyjs.com>
2021-03-02 11:32:58 -0800

    feat: style the Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText
+} from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <main>
+    <main className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
         </ul>
       </nav>
-      <h1>{pageTitle}</h1>
+      <h1 className={heading}>{pageTitle}</h1>
       {children}
     </main>
   )
