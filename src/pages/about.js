43cc16463cffef24cccbcad263889b32a1ce59c8
Megan Sullivan <megan@gatsbyjs.com>
2021-03-02 11:15:21 -0800

    feat: add navigation links to home and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <main>
       <title>About Me</title>
       <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
       <p>My name is Megan. I like teaching, video games, and dogs!</p>
     </main>
   )
