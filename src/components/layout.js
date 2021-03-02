83996e21e5b60d5684c6212065466e2291ae3931
Megan Sullivan <megan@gatsbyjs.com>
2021-03-02 11:58:57 -0800

    feat: add site title to Layout (with useStaticQuery)

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,30 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `)
+
   return (
     <main className={container}>
-      <title>{pageTitle}</title>
+      <title>{data.site.siteMetadata.title}</title>
+      <p className={siteTitle}>{data.site.siteMetadata.title}</p>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
